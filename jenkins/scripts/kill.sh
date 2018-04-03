8e5960a3868b587fc988f2d7cb5278316763f37c
mdsauce <max.dobeck@saucelabs.com>
2018-04-03 15:59:23 -0700

    updated kill.sh for more verbosity and cleaned up bad output.

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -3,8 +3,9 @@
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
+echo 'current node process'
 set -x
-echo 'current node process' 
 ps -ax | grep npm
-cat .pidfile
+
+$(cat .pidfile)
 kill $(cat .pidfile)
