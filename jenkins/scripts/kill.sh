fa5cb6cef735df1423d3327da446d42213932129
mdsauce <max.dobeck@saucelabs.com>
2018-04-03 15:31:15 -0700

    Trying more verbose kill script

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -4,5 +4,6 @@ echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
 set -x
-echo ps -ax | grep node
+echo 'current node process' ps -ax | grep node
+echo $(cat .pidfile)
 kill $(cat .pidfile)
