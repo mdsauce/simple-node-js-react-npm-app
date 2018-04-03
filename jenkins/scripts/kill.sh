a3ca41a97eed32177d2c516db04949d07f4845c6
mdsauce <max.dobeck@saucelabs.com>
2018-04-03 15:35:07 -0700

    Had wrong script syntaxt for kill.sh

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -4,6 +4,7 @@ echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
 set -x
-echo 'current node process' ps -ax | grep node
-echo $(cat .pidfile)
+echo 'current node process' 
+ps -ax | grep node
+cat .pidfile
 kill $(cat .pidfile)
