a1194128ec3718b82e8e45c35eea03cbe6a9165f
mdsauce <max.dobeck@saucelabs.com>
2018-04-03 16:07:59 -0700

    Less verbose kill.sh file.

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -3,8 +3,7 @@
 echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
-echo 'current node process'
-set -x
+set +x
 ps -ax | grep npm
 
 $(cat .pidfile)
